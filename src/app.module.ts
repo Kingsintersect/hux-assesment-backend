2ff87b219279b5ec750b7e3b2d2524b1416ef75d
Kingsintersect <kingsintersect@gmail.com>
2024-05-04 07:19:09 -0700

    generated contacts resource files for managing user contacts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule, DuplicateKeyFilter } from './common';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
   imports: [
     DatabaseModule,
     AuthModule,
+    ContactsModule,
   ],
   controllers: [],
   providers: [
